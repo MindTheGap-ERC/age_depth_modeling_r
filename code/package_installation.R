3bf4e91428a593224e62dd6c9853dca68f43b3c0
Hohmann <n.h.hohmann@uu.nl>
2025-06-07 16:04:22 +0200

    install mandatory packages

---
+++ code/package_installation.R
@@ -0,0 +1,9 @@
+install.packages("remotes")
+
+remotes::install_github(repo = "robintrayler/modifiedBChron")
+remotes::install_github(repo = "andrewcparnell/Bchron")
+
+remotes::install_github(repo = "robintrayler/astroBayes")
+
+install.packages("admtools")
+
